2b3395d26ae86200ec0677f565c805232d5d1863
Grzegorz Kluka <g.kluka@wp.pl>
2017-11-25 08:24:34 +0100

    Added JS

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+var a=prompt("podaj szerokość");
+var h=prompt("podaj wysokość");
+	triangleArea = a*h/2;
+alert('Pole trójkąta o szerokości a = ' + a + ' i wysokości h = ' + h + ' wynosi: ' + triangleArea);
